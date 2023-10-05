4b3f44993529b274125b3704b63d98369281e187
DeveloperDurp <DeveloperDurp@durp.info>
2023-10-05 07:43:45 -0500

    add Windows check

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
-import "winutil-cli/cmd"
+import (
+	"fmt"
+	"runtime"
+	"winutil-cli/cmd"
+)
 
 func main() {
+	if runtime.GOOS != "windows" {
+		fmt.Println("This is a Windows Only Utility")
+		return
+	}
 	cmd.Execute()
 }
-
