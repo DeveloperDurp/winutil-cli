f1b2c986ed6869e1ec7af8288ac29fcc4d2853a7
DeveloperDurp <DeveloperDurp@durp.info>
2023-10-07 06:22:15 -0500

    remove config section

--- cmd/root.go
+++ cmd/root.go
@@ -1,13 +1,11 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	"winutil-cli/cmd/app"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var rootCmd = &cobra.Command{
@@ -23,36 +21,9 @@ func Execute() {
 	}
 }
 
-func setDefaults() {
-}
-
 func init() {
-	home, err := os.UserHomeDir()
-	cobra.CheckErr(err)
-	cobra.OnInitialize(initConfig)
-
-	setDefaults()
-	cfgFile := home + "/.winutil.yaml"
-	err = viper.WriteConfigAs(cfgFile)
-	if err != nil {
-		fmt.Println(err)
-	}
 
 	rootCmd.AddCommand(app.Appcmd)
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.winutil.yaml)")
 
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-func initConfig() {
-	home, err := os.UserHomeDir()
-	cobra.CheckErr(err)
-
-	viper.AddConfigPath(home)
-	viper.SetConfigType("yaml")
-	viper.SetConfigName(".winutil")
-
-	viper.AutomaticEnv()
-	viper.ReadInConfig()
-
 }
